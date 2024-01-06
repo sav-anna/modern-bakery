cf097abf52ae100ac03a98d1bc29f3fb4e7fca68
sav-anna <anna_sibova@outlook.com>
2024-01-06 11:03:39 +0000

    Deploying to gh-pages from @ sav-anna/modern-bakery@3d38105925210c0729b5c14921737f9eef6ba366 🚀

--- index.114c5ea5.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".backdrop"),t=document.querySelector(".btn-order"),c=document.querySelector(".modal__close-btn"),d=()=>e.classList.toggle("is-hidden");t.addEventListener("click",d),c.addEventListener("click",d);
-//# sourceMappingURL=index.114c5ea5.js.map
--- index.c39eb2e8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".backdrop"),t=document.querySelector(".btn-order"),c=document.querySelector(".modal__close-btn"),n=function(){return e.classList.toggle("is-hidden")};t.addEventListener("click",n),c.addEventListener("click",n)}();
-//# sourceMappingURL=index.c39eb2e8.js.map
