3d38105925210c0729b5c14921737f9eef6ba366
Anna Sibova <anna_sibova@outlook.com>
2024-01-06 13:03:12 +0200

    add slider

---
+++ src/js/slider.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button.next',
+    prevEl: '.swiper-button.prev',
+  },
+});
