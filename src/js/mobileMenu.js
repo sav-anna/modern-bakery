5245be4cc5c8fb668065e97b2676c8525d016aa4
Anna Sibova <anna_sibova@outlook.com>
2024-03-31 22:52:25 +0300

    fixMenu

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,8 +1,12 @@
 const mobileMenu = document.querySelector('.mobileMenu');
 const mobileBtnOpen = document.querySelector('.header-nav__burger-menu');
 const mobileBtnClose = document.querySelector('.mobileMenu__close');
+const mobileMenu__link = document.querySelectorAll('.mobileMenu__link');
 
 const toggleMobileMenu = () => mobileMenu.classList.toggle('is-hiddenMenu');
 
+const closeMobileMenu = () => mobileMenu.classList.add('is-hiddenMenu');
+
 mobileBtnOpen.addEventListener('click', toggleMobileMenu);
 mobileBtnClose.addEventListener('click', toggleMobileMenu);
+mobileMenu__link.addEventListener('touchstart', closeMobileMenu);
